cd323695243b7f52a0ddfda31afa580b7333b4ea
mbuscemi <matthew.buscemi@gmail.com>
2020-04-18 18:56:28 -0700

    DiskEntry stores the isolated filename as well; only the filename is displayed in the project hierarchy

--- shared/src/disk_entry.rs
+++ shared/src/disk_entry.rs
@@ -5,15 +5,19 @@ use yew::{ html, Html };
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct DiskEntry {
     full_path: String,
+    filename: String,
     path_in_project: String,
     is_dir: bool,
 }
 
 impl DiskEntry {
     pub fn new(file_path: String, project_path: String) -> Self {
+        let reversed_filename: String = file_path.chars().rev().take_while(|&c| c != '/').collect();
+        let filename: String = reversed_filename.chars().rev().collect();
 
         DiskEntry {
             full_path: file_path.clone(),
+            filename: filename,
             path_in_project: file_path.clone().replace(&project_path, ""),
             is_dir: is_dir(file_path),
         }
@@ -22,11 +26,11 @@ impl DiskEntry {
     pub fn render(&self) -> Html {
         if self.is_dir {
             html! {
-                <li class="dir">{self.path_in_project.clone()}</li>
+                <li class="dir">{self.filename.clone()}</li>
             }
         } else {
             html! {
-                <li class="file">{self.path_in_project.clone()}</li>
+                <li class="file">{self.filename.clone()}</li>
             }
         }
     }
