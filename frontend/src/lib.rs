78e07505874421da6e44fbd8a12413b5c5b14ebf
mbuscemi <matthew.buscemi@gmail.com>
2020-04-19 11:46:22 -0700

    (refactor) moved large chunks of the view code into a new view module

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -10,8 +10,8 @@ mod event;
 mod file;
 mod message;
 pub mod model;
+mod view;
 
-use rocksalt_shared::disk_entry::DiskEntry;
 use yew::{html, Component, ComponentLink, Html, ShouldRender};
 
 use event::{
@@ -74,47 +74,12 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
-        let (header, project_contents) = match &self.project_path {
-            Some(path) => (
-                html! {
-                    <header>
-                        <span id="project-path">
-                            {path}
-                        </span>
-                        <button id="close-button" onclick=self.link.callback(|_| Message::CloseProject)>
-                            { "✖" }
-                        </button>
-                    </header>
-                },
-                html! {
-                    <>
-                        <h1 class="active">{ "Project" }</h1>
-                        {project_hierarchy(&self.link, &self.project_structure)}
-                    </>
-                }
-            ),
-            None => (
-                html! {
-                    <header>
-                        // TODO: support opening a file through the project explorer
-                        // <button onclick=self.link.callback(|_| Message::OpenFile)>{ "Open File" }</button>
-                        <button id="open-project-folder-button" onclick=self.link.callback(|_| Message::OpenProject)>
-                            { "Open Project Folder" }
-                        </button>
-                    </header>
-                },
-                html! {
-                    <h1 class="inactive">{ "Project" }</h1>
-                }
-            ),
-        };
-
         html! {
             <div id="page">
-                {header}
+                {self.header()}
                 <section id="main-editor">
                     <div id="project-panel">
-                        {project_contents}
+                        {self.project_contents()}
                     </div>
                     <div id="editor">
                         { &self.file.contents }
@@ -123,42 +88,5 @@ impl Component for Model {
                 <footer></footer>
             </div>
         }
-    }
-}
-
-fn project_hierarchy(link: &ComponentLink<Model>, project_structure: &Option<Vec<DiskEntry>>) -> Html {
-    match project_structure {
-        Some(structure) => {
-            let mut mut_structure = structure.clone();
-            let top_dir = mut_structure.remove(0);
-
-            html! { <ul>{ render_dir(link, top_dir, &mut mut_structure) }</ul> }
-        },
-        None => html! {},
-    }
-}
-
-//TODO: figure out why deeply nested files aren't displaying
-fn render_dir(link: &ComponentLink<Model>, top_dir: DiskEntry, rest: &mut Vec<DiskEntry>) -> Html {
-    let top_dir_clone = top_dir.clone();
-    let top_dir_project_path = format!("{}{}", top_dir.path_in_project, std::path::MAIN_SEPARATOR);
-
-    let (this_dir_entries, other_entries): (Vec<DiskEntry>, Vec<DiskEntry>)
-        = rest.drain(..).partition(|entry| entry.project_path_sans_filename() == top_dir_project_path );
-
-    let (mut these_folders, mut these_files): (Vec<DiskEntry>, Vec<DiskEntry>)
-        = this_dir_entries.into_iter().partition(|entry| entry.is_dir() );
-
-    these_folders.sort_by(|a, b| a.filename.to_lowercase().cmp(&b.filename.to_lowercase()));
-    these_files.sort_by(|a, b| a.filename.to_lowercase().cmp(&b.filename.to_lowercase()));
-
-    html! {
-        <li class={ format!("{} {}", top_dir.css_class(), if top_dir.active { "active" } else { "" }) }>
-            <span onclick=link.callback(move |_| Message::ToggleHierarchy(top_dir_clone.full_path.clone()))>{ top_dir.filename }</span>
-            <ul>
-                { these_folders.iter().map(|entry| render_dir(link, entry.clone(), &mut other_entries.clone())).collect::<Html>() }
-                { these_files.iter().map(|entry| html! { <li class={entry.css_class()}><span>{entry.filename.clone()}</span></li> }).collect::<Html>() }
-            </ul>
-        </li>
     }
 }
---
+++ frontend/src/view.rs
@@ -0,0 +1,90 @@
+use rocksalt_shared::disk_entry::DiskEntry;
+use yew::{html, Html};
+
+use crate::message::Message;
+use crate::model::Model;
+
+impl Model {
+    pub fn header(&self) -> Html {
+        match &self.project_path {
+            Some(path) => (
+                html! {
+                    <header>
+                        <span id="project-path">
+                            {path}
+                        </span>
+                        <button id="close-button" onclick=self.link.callback(|_| Message::CloseProject)>
+                            { "✖" }
+                        </button>
+                    </header>
+                }
+            ),
+            None => (
+                html! {
+                    <header>
+                        // TODO: support opening a file through the project explorer
+                        // <button onclick=self.link.callback(|_| Message::OpenFile)>{ "Open File" }</button>
+                        <button id="open-project-folder-button" onclick=self.link.callback(|_| Message::OpenProject)>
+                            { "Open Project Folder" }
+                        </button>
+                    </header>
+                }
+            ),
+        }
+    }
+
+    pub fn project_contents(&self) -> Html {
+        match &self.project_path {
+            Some(_) => (
+                html! {
+                    <>
+                        <h1 class="active">{ "Project" }</h1>
+                        {self.project_hierarchy()}
+                    </>
+                }
+            ),
+            None => (
+                html! {
+                    <h1 class="inactive">{ "Project" }</h1>
+                }
+            ),
+        }
+    }
+
+    pub fn project_hierarchy(&self) -> Html {
+        match &self.project_structure {
+            Some(structure) => {
+                let mut mut_structure = structure.clone();
+                let top_dir = mut_structure.remove(0);
+
+                html! { <ul>{ self.render_dir(top_dir, &mut mut_structure) }</ul> }
+            },
+            None => html! {},
+        }
+    }
+
+    //TODO: figure out why deeply nested files aren't displaying
+    pub fn render_dir(&self, top_dir: DiskEntry, rest: &mut Vec<DiskEntry>) -> Html {
+        let top_dir_clone = top_dir.clone();
+        let top_dir_project_path = format!("{}{}", top_dir.path_in_project, std::path::MAIN_SEPARATOR);
+
+        let (this_dir_entries, other_entries): (Vec<DiskEntry>, Vec<DiskEntry>)
+            = rest.drain(..).partition(|entry| entry.project_path_sans_filename() == top_dir_project_path );
+
+        let (mut these_folders, mut these_files): (Vec<DiskEntry>, Vec<DiskEntry>)
+            = this_dir_entries.into_iter().partition(|entry| entry.is_dir() );
+
+        these_folders.sort_by(|a, b| a.filename.to_lowercase().cmp(&b.filename.to_lowercase()));
+        these_files.sort_by(|a, b| a.filename.to_lowercase().cmp(&b.filename.to_lowercase()));
+
+        html! {
+            <li class={ format!("{} {}", top_dir.css_class(), if top_dir.active { "active" } else { "" }) }>
+                <span onclick=self.link.callback(move |_| Message::ToggleHierarchy(top_dir_clone.full_path.clone()))>{ top_dir.filename }</span>
+                <ul>
+                    { these_folders.iter().map(|entry| self.render_dir(entry.clone(), &mut other_entries.clone())).collect::<Html>() }
+                    { these_files.iter().map(|entry| html! { <li class={entry.css_class()}><span>{entry.filename.clone()}</span></li> }).collect::<Html>() }
+                </ul>
+            </li>
+        }
+    }
+}
