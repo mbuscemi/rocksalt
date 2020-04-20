89ffe5394e33593d540dfcf7351c5ae7d2e5716b
mbuscemi <matthew.buscemi@gmail.com>
2020-04-20 12:32:07 -0700

    (refactor) created another util function to make event_handler even cleaner

--- shared/src/utils.rs
+++ shared/src/utils.rs
@@ -6,3 +6,12 @@ pub fn on_some<T, F>(option: Option<T>, f: F) -> ()
         None => (),
     }
 }
+
+pub fn on_ok<T, E, F>(result: Result<T, E>, f: F) -> ()
+    where F: FnOnce(T) -> ()
+{
+    match result {
+        Ok(x) => { f(x); () },
+        Err(_) => ()
+    }
+}
--- src/event_handler.rs
+++ src/event_handler.rs
@@ -6,38 +6,40 @@ use rocksalt_shared::event::{
     set_project_path::SetProjectPath
 };
 use rocksalt_shared::file_system::disk_entry::DiskEntry;
-use rocksalt_shared::utils::on_some;
+use rocksalt_shared::utils::{ on_ok, on_some };
 
 use crate::file;
 use crate::message::Message;
 use crate::rpc;
 
 pub fn handle(webview: &mut WebView<()>, arg: &str) -> WVResult {
-    match serde_json::from_str(arg) {
-        Ok(message) => {
-            match message {
-                Message::OpenFile => {
-                    on_some(tfd::open_file_dialog("Open File", "", None), |path| {
-                        rpc::dispatch(webview, SetFile{ contents: file::read(&path) });
-                    });
-                    Ok(())
-                },
+    on_ok(serde_json::from_str(arg), |message| {
+        match message {
+            Message::OpenFile => {
+                on_some(
+                    tfd::open_file_dialog("Open File", "", None),
+                    |path| {
+                        rpc::dispatch(webview, SetFile {
+                            contents: file::read(&path)
+                        });
+                    }
+                );
+            },
 
-                Message::OpenProject => {
-                    on_some(tfd::select_folder_dialog("Open Project Folder", ""), |path| {
+            Message::OpenProject => {
+                on_some(
+                    tfd::select_folder_dialog("Open Project Folder", ""),
+                    |path| {
                         let dir_structure: Vec<DiskEntry> = file::dir_structure(&path);
-                        rpc::dispatch(webview, SetProjectPath{
+                        rpc::dispatch(webview, SetProjectPath {
                             path: path,
                             dir_structure: dir_structure
                         });
-                    });
-                    Ok(())
-                }
-            }
-        }
-        Err(error) => {
-            println!("Could not match event from webview: {}", error);
-            Ok(())
-        }
-    }
+                    }
+                );
+            },
+        };
+    });
+
+    Ok(())
 }
